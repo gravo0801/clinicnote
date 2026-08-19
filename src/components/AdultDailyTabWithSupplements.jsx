e333ae3c414dd9e8e693ef5bdd111d2d6b016744
gravo0801 <fnaticdoc@gmail.com>
2026-08-19 09:20:23 +0900

    Register Adult Daily Day 11-15 study cards
    * Add Adult Daily Day 12-15 clinical cards
    
    * Register Adult Daily Day 11-15 cards
    
    * Load Adult Daily supplemental cards
    
    * Avoid unavailable PDF links for Day 12-15 cards

--- src/components/AdultDailyTabWithSupplements.jsx
+++ src/components/AdultDailyTabWithSupplements.jsx
@@ -1,9 +1,13 @@
 import AdultDailyTab from './AdultDailyTab'
 import { adultDailyContent } from '../data/adultDailyContent'
 import { adultDailyDay11 } from '../data/adultDailyDay11'
+import { adultDailyDay12to15 } from '../data/adultDailyDay12to15'
+
+const day12to15Cards = adultDailyDay12to15.map(({ pdfPath, ...card }) => card)
 
 const supplementalCards = [
   ...adultDailyDay11,
+  ...day12to15Cards,
 ]
 
 supplementalCards.forEach(card => {
